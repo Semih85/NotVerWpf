d3eacf429ece82261e1f768e403d9db0e7917898
Semih ÖZDAMAR <ozdamarsem85@hotmail.com>
2019-05-29 23:13:29 +0300

    array ile arka planda işlemler yapıldıktan sonra sonuçlar excele aktarılıyor.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -39,80 +39,171 @@ namespace NotVerWpf
                 var row = excel.AktifSatir;
                 var col = excel.AktifSutun;
                 var enAzNot = Convert.ToInt32(txtEnAzNot.Text);
+                var katsayi = Convert.ToInt32(txtKatsayi.Text);
+                var ogrenciSayisi = excel.ToplamSatirSayisi - excel.AktifSatir + 1;
+                var notlar = new int[ogrenciSayisi, excel.KriterSayisi + 1];
+                var r = new Random();
+                var r2 = row;
 
-                do
+                if (excel.KriterlerinToplami != 100)
                 {
-                    var toplaminEsitlenecegiDeger = excel.ReadCellInt(row, excel.ToplaminEsitlenecegiSutun);
-                    //excel.ToplamNot = 0;
+                    MessageBox.Show("Kriterlerin toplamı 100 olmalıdır.");
+                    return;
+                }
+                
+                for (int i = 0; i < ogrenciSayisi; i++)
+                {
+                    var toplaminEsitlenecegiDeger = excel.ReadCellInt(r2, excel.ToplaminEsitlenecegiSutun);
+                    var c2 = col;
 
-                    NotlariRasgeleDagit(excel, row, enAzNot, toplaminEsitlenecegiDeger);
-
-                    excel.WriteCell(row, excel.ToplamSutunu, excel.ToplamNot);
+                    for (int j = 0; j < excel.KriterSayisi; j++)
+                    {
+                        var a = enAzNot;
+                        var b = excel.ReadCellInt(excel.KriterSatiri, c2);
+                        var hesaplanacakSayi = Math.Round(r.NextDouble() * (b - a) + a, 0);
+                        var modHesapla = hesaplanacakSayi % katsayi;
+                        if (modHesapla > 0)
+                        {
+                            do
+                            {
+                                hesaplanacakSayi--;
+                                modHesapla = hesaplanacakSayi % katsayi;
+                            } while (modHesapla != 0);
+                        }
+                        notlar[i, j] = (int)hesaplanacakSayi;
+                        notlar[i, excel.KriterSayisi] += (int)hesaplanacakSayi;//toplam
+                        c2++;
+                    }
 
                     if (toplaminEsitlenecegiDeger < enAzNot * excel.KriterSayisi)
                     {
-                        MessageBox.Show($"Eşitlenecek not en az {enAzNot * excel.KriterSayisi} olmalıdır. ({toplaminEsitlenecegiDeger})");
+                        MessageBox.Show($"Eşitlenecek not en az {enAzNot * excel.KriterSayisi} olmalıdır. (Girilen Not: {toplaminEsitlenecegiDeger})");
                         return;
                     }
 
-                    var h = col;
+                    var h = 0;
+                    var c3 = col;
 
-                    if (excel.ToplamNot < toplaminEsitlenecegiDeger)
+                    if (notlar[i, excel.KriterSayisi] < toplaminEsitlenecegiDeger)
                     {
                         do
                         {
-                            //if (h < excel.ToplamSutunu)
-                            //{
-                            var artacakSayi = excel.ReadCellInt(row, h);
-
-                            artacakSayi += 5;
-                            excel.ToplamNot += 5;
-
-                            excel.WriteCell(row, h, artacakSayi);
-                            excel.WriteCell(row, excel.ToplamSutunu, excel.ToplamNot);
-                            //}
-
-                            if (h == excel.ToplamSutunu)
+                            if (notlar[i, h] < excel.ReadCellInt(excel.KriterSatiri, c3))
                             {
-                                h = col;
+                                notlar[i, h] += katsayi;
+                                notlar[i, excel.KriterSayisi] += katsayi;
+                            }
+
+                            if (h == excel.KriterSayisi - 1)
+                            {
+                                h = 0;
+                                c3 = col;
                             }
                             else
                             {
                                 h++;
+                                c3++;
                             }
 
-                        } while (excel.ToplamNot < toplaminEsitlenecegiDeger);
+                        } while (notlar[i, excel.KriterSayisi] < toplaminEsitlenecegiDeger);
                     }
                     else
                     {
                         do
                         {
-                            var azlacakSayi = excel.ReadCellInt(row, h);
-
-                            if (azlacakSayi > enAzNot)
+                            if (notlar[i, h] > enAzNot)
                             {
-                                azlacakSayi -= 5;
-                                excel.ToplamNot -= 5;
-
-                                excel.WriteCell(row, h, azlacakSayi);
-                                excel.WriteCell(row, excel.ToplamSutunu, excel.ToplamNot);
+                                notlar[i, h] -= katsayi;
+                                notlar[i, excel.KriterSayisi] -= katsayi;
                             }
 
-                            if (h == excel.ToplamSutunu)
+                            if (h == excel.KriterSayisi - 1)
                             {
-                                h = col;
+                                h = 0;
                             }
                             else
                             {
                                 h++;
                             }
 
-                        } while (excel.ToplamNot > toplaminEsitlenecegiDeger);
+                        } while (notlar[i, excel.KriterSayisi] > toplaminEsitlenecegiDeger);
                     }
 
-                    row++;
+                    r2++;
+                }
 
-                } while (excel.ReadCellString(row, excel.AktifSutun - 1) != "");
+                excel.WriteRange(excel.AktifSatir, excel.AktifSutun, excel.ToplamSatirSayisi, excel.ToplamSutunSayisi + 1, notlar);
+
+                //do
+                //{
+                //    var toplaminEsitlenecegiDeger = excel.ReadCellInt(row, excel.ToplaminEsitlenecegiSutun);
+                //    //excel.ToplamNot = 0;
+
+                //    NotlariRasgeleDagit(excel, row, enAzNot, toplaminEsitlenecegiDeger);
+
+                //    excel.WriteCell(row, excel.ToplamSutunu, excel.ToplamNot);
+
+                //    if (toplaminEsitlenecegiDeger < enAzNot * excel.KriterSayisi)
+                //    {
+                //        MessageBox.Show($"Eşitlenecek not en az {enAzNot * excel.KriterSayisi} olmalıdır. ({toplaminEsitlenecegiDeger})");
+                //        return;
+                //    }
+
+                //    var h = col;
+
+                //    if (excel.ToplamNot < toplaminEsitlenecegiDeger)
+                //    {
+                //        do
+                //        {
+                //            var artacakSayi = excel.ReadCellInt(row, h);
+
+                //            artacakSayi += 5;
+                //            excel.ToplamNot += 5;
+
+                //            excel.WriteCell(row, h, artacakSayi);
+                //            excel.WriteCell(row, excel.ToplamSutunu, excel.ToplamNot);
+
+                //            if (h == excel.ToplamSutunu)
+                //            {
+                //                h = col;
+                //            }
+                //            else
+                //            {
+                //                h++;
+                //            }
+
+                //        } while (excel.ToplamNot < toplaminEsitlenecegiDeger);
+                //    }
+                //    else
+                //    {
+                //        do
+                //        {
+                //            var azlacakSayi = excel.ReadCellInt(row, h);
+
+                //            if (azlacakSayi > enAzNot)
+                //            {
+                //                azlacakSayi -= 5;
+                //                excel.ToplamNot -= 5;
+
+                //                excel.WriteCell(row, h, azlacakSayi);
+                //                excel.WriteCell(row, excel.ToplamSutunu, excel.ToplamNot);
+                //            }
+
+                //            if (h == excel.ToplamSutunu)
+                //            {
+                //                h = col;
+                //            }
+                //            else
+                //            {
+                //                h++;
+                //            }
+
+                //        } while (excel.ToplamNot > toplaminEsitlenecegiDeger);
+                //    }
+
+                //    row++;
+
+                //} while (excel.ReadCellString(row, excel.AktifSutun - 1) != "");
 
                 MessageBox.Show("Notlar başarı ile verildi.");
                 //lblSonuc.Content = "Notlar başarı ile verildi.";
@@ -122,7 +213,6 @@ namespace NotVerWpf
         private void NotlariRasgeleDagit(ExcelProgramla excel, int row, int enAzNot, int toplaminEsitlenecegiDeger)
         {
             var r = new Random();
-
             excel.ToplamNot = 0;
 
             for (int i = excel.AktifSutun; i < excel.ToplamSutunu; i++)
@@ -195,13 +285,6 @@ namespace NotVerWpf
                 {
                     KriterlerinToplami += ReadCellInt(KriterSatiri, i);
                 }
-
-                if (KriterlerinToplami != 100)
-                {
-                    MessageBox.Show("Kriterlerin toplamı 100 olmalıdır.");
-                    return;
-                    //throw new Exception("Kriterlerin toplamı 100 olmalıdır.");
-                }
             }
             private void ToplamSutunSayisiHesapla()
             {
@@ -263,15 +346,41 @@ namespace NotVerWpf
                     return Convert.ToString(deger);
                 }
             }
+            public string[,] ReadCellString(int starti, int starty, int endi, int endy)
+            {
+                var range = (Range)WorkSheet.Range[WorkSheet.Cells[starti, starty], WorkSheet.Cells[endi, endy]];
+                object[,] holder = range.Value2;
+                string[,] returnString = new string[endi - starti + 1, endy - starty + 1];
+
+                for (int i = 0; i < endi - starti + 1; i++)
+                {
+                    for (int j = 0; j < endy - starty + 1; j++)
+                    {
+                        returnString[i, j] = holder[i, j].ToString();
+                    }
+                }
+
+                return returnString;
+            }
+            public void WriteRange(int starti, int starty, int endi, int endy, int[,] writeInt)
+            {
+                var range = (Range)WorkSheet.Range[WorkSheet.Cells[starti, starty], WorkSheet.Cells[endi, endy]];
+                range.Value2 = writeInt;
+            }
 
             public void WriteCell(int i, int j, double deger)
             {
-                WorkSheet.Cells[i, j].Value = deger;
+                WorkSheet.Cells[i, j].Value2 = deger;
             }
 
             public void WriteCell(int i, int j, string deger)
             {
                 WorkSheet.Cells[i, j].Value2 = deger;
+            }
+
+            public void Save()
+            {
+                WorkBook.Save();
             }
 
             //public void OpenFile(string path)
